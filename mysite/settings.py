2d8cc958c46fdbb9de82b86867d902cc687a7eda
Michael Ayeni <ayenimichael92@gmail.com>
2025-02-11 23:10:34 +0100

    minor changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {    
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('PGDATABASE')
+        'NAME': os.getenv('PGDATABASE'),
         'USER':  os.getenv('PGUSER'),
         'PASSWORD': os.getenv('PGPASSWORD'),
         'HOST': os.getenv('PGHOST'),
