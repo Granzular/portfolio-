b63312eda3d1035af47774f943af5c2a252f9999
granzular <ayenimichael92@gmail.com>
2025-05-31 01:19:48 +0100

    secured smtp

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -57,7 +57,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # EMAILING SETTINGS
 
 EMAIL_HOST_USER = "granzularcodex@gmail.com"
-EMAIL_HOST_PASSWORD = "acfq cpgg cosl egsh"
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
