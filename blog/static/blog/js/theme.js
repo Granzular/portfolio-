a5d839c84d789fcd0a048b3e76556349b8107331
granzular <ayenimichael92@gmail.com>
2025-08-07 23:15:35 +0100

    dark theme javascript code refactored. enabled localstorage of theme

--- blog/static/blog/js/theme.js
+++ blog/static/blog/js/theme.js
@@ -1,19 +1,44 @@
-window.addEventListener("load",function(e){
-    const theme_link = document.querySelector("#dark-theme");
+window.addEventListener("load",registerEvents,false);
+
+function registerEvents(e){
     
     const toggle_icon = document.querySelector("#theme-toggle-icon");
-    toggle_icon.addEventListener("click",change_theme);
-    function change_theme(e){
-        e.target.classList.toggle("dark-theme");
-        if (e.target.classList.contains("dark-theme")){
-            e.target.innerHTML = "☀️";
+    
+    const theme_link = document.querySelector("#dark-theme");//link tag for dark theme css
+    init_theme(localStorage.getItem("theme"));
+    toggle_icon.addEventListener("click",toggle_theme,false);
+    
+    /* helper functions definitions */
+    
+    function init_theme(theme){
+        if (theme=="dark-theme"){
             theme_link.disabled = false;
+            toggle_icon.innerHTML = "🌞";
         }
-        else{
-         e.target.innerHTML =  "🌙";
+        else if(theme == "light-theme"){
             theme_link.disabled = true;
+            toggle_icon.innerHTML = "🌙";
+        }
+        else if(theme == null){
+            localStorage.setItem("theme","light-theme");
+            theme_link.disabled = true;
+            toggle_icon.innerHTML = "🌙";
+        }
+    }
+    
+    function toggle_theme(e){
+        
+        let theme = localStorage.getItem("theme");
+        
+        if (theme == "dark-theme"){
+            theme_link.disabled = true;
+            toggle_icon.innerHTML = "🌙";
+            localStorage.setItem("theme","light-theme")
+        }
+        else if(theme == "light-theme"){
+            theme_link.disabled = false;
+            toggle_icon.innerHTML = "🌞";
+            localStorage.setItem("theme","dark-theme");
         }
     }
 }
-);
-
