a39275c5550cd5e65bee3a0ec1d36c06fe1222a5
Granzular <ayenimichael92@gmail.com>
2025-12-11 22:23:31 +0100

    some test update

---
+++ blog/static/blog/js/main.js
@@ -0,0 +1,14 @@
+window.addEventListener("load",(e)=>{
+
+	const observer = new IntersectionObserver((entries) => { entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("show");
+    } 
+		else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+document.querySelectorAll(".anim").forEach((el) => observer.observe(el));
+},false);
